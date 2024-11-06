061589ec82ef7bbc2be026a2091c1460bd89904e
Greg Robleto <greg@robleto.com>
2024-11-06 07:13:41 -0500

    Fix duplicate import of Logo component in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import type { Metadata } from "next";
 import Logo from "./_components/layout/sidebar/Logo";
+import Logo from "./_components/layout/sidebar/Logo";
 import "./_style/globals.css"; // Import global styles
 import { Nunito_Sans, Oswald, Libre_Bodoni, Inter } from "next/font/google";
 
