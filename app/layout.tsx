f59d8ac0731b0d16e2a06e73d099ac7c88e96682
Greg Robleto <greg@robleto.com>
2024-11-11 17:01:18 -0500

    Refactor global styles and layout structure; update background and font integration

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,28 +1,9 @@
 import React from "react";
 import type { Metadata } from "next";
 import "./_style/globals.css"; // Import global styles
-import SideNav from "./_components/layout/sidebar/SideNav"; // Import SideNav
-import { Nunito_Sans, Oswald, Libre_Bodoni } from "next/font/google";
-import { ThemeProvider } from "next-themes"; // Import ThemeProvider for theme switching
+import { Nunito_Sans, Oswald, Libre_Bodoni, Inter } from "next/font/google";
 
-// Import fonts...
-const libreBodoni = Libre_Bodoni({
-	subsets: ["latin"],
-	weight: ["400", "500", "600", "700"],
-	variable: "--font-libre-bodoni",
-});
-
-const nunitoSans = Nunito_Sans({
-	subsets: ["latin"],
-	weight: ["300", "400", "500", "600", "700"],
-	variable: "--font-nunito-sans",
-});
-
-const oswald = Oswald({
-	subsets: ["latin"],
-	weight: ["400", "500", "600", "700"],
-	variable: "--font-oswald",
-});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
 	title: "Greg Robleto | Greg Robleto – Design / Marketing / Product / Technology ",
@@ -32,28 +13,12 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
 	children,
-}: {
+}: Readonly<{
 	children: React.ReactNode;
-}) {
+}>) {
 	return (
 		<html lang="en">
-			<head></head>
-			<body
-				className={`${nunitoSans.variable} ${oswald.variable} ${libreBodoni.variable} antialiased bg-background dark:bg-dark_background`}
-			>
-				{/* Wrapping the app in ThemeProvider for theme toggling */}
-				<ThemeProvider attribute="class" defaultTheme="system">
-					<div className="flex min-h-screen">
-						{/* SideNav is collapsed by default on mobile and expanded on larger screens */}
-						<SideNav />
-						{/* Main content */}
-						<main className="flex-grow max-w-screen-xl pl-[20%] w-[80%] md:pl-[25%] lg:w-auto mx-auto p-8 transition-all duration-300 dark:text-white">
-							{children}
-						</main>
-						
-					</div>
-				</ThemeProvider>
-			</body>
+			<body className={inter.className}>{children}</body>
 		</html>
 	);
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,92 +1,11 @@
 import React from "react";
-import { fetchNotionData } from "@/lib/notionContentFetcher";
-import Lists from "@/app/_components/views/list/List";
-import { sortByPinnedAndDate } from "@/utils/sortItems";
-import FlippingWords from "@/app/_components/custom/FlippingWords";
-import Gallery from "@/app/_components/views/gallery/Gallery";
-import GroupTitle from "@/app/_components/views/common/GroupTitle";
-import MiniCardView from "@/app/_components/views/mini-card/MiniCardSet";
 
-export default async function HomePage() {
-	// Fetch data using the refactored fetchNotionData function
-	const { listItems: aboutItems } = await fetchNotionData({
-		databaseId: process.env.NOTION_ABOUT_DB_ID!,
-		entryType: "about",
-	});
-
-	const { listItems: postItems } = await fetchNotionData({
-		databaseId: process.env.NOTION_POSTS_DB_ID!,
-		entryType: "posts",
-	});
-
-	const { listItems: readingListItems } = await fetchNotionData({
-		databaseId: process.env.NOTION_READINGLIST_DB_ID!,
-		entryType: "reading-list",
-	});
-
-	// Sort and limit the data
-	const limitedHomeItems = sortByPinnedAndDate(aboutItems, "date").slice(0, 4);
-	const postItemsSorted = sortByPinnedAndDate(postItems, "pubdate");
-	const firstTwoBlogPosts = postItemsSorted.slice(0, 2);
-	const firstThreeReadingListPosts = readingListItems.slice(0,4);
-
+const Page: React.FC = () => {
 	return (
 		<div className="container mx-auto p-4">
-			<h1 className="text-2xl font-bold hidden">Greg Robleto</h1>
-
-			<section className="w-full max-w-6xl mx-auto py-8">
-				<div className="w-full flex items-baseline">
-					<img
-						src="/home/home-gregrobleto.svg"
-						alt="Greg Robleto"
-						className="w-full h-auto"
-					/>
-				</div>
-
-				<FlippingWords />
-
-				<div className="flex justify-center items-center mt-2">
-					<img
-						src="/home/home-rockvillemd.svg"
-						alt="Rockville, MD"
-						className="w-full md:w-1/2 h-auto"
-					/>
-				</div>
-			</section>
-
-			<GroupTitle title="Updates" />
-
-			{/* Render the limited home items list */}
-			<Lists
-				items={limitedHomeItems}
-				pageKey="about"
-				linkKey="url"
-				descriptionKey="description"
-				pubDateKey="date"
-				titleKey="name"
-				tagsKey="tags"
-				slugKey="slug"
-			/>
-
-			<GroupTitle title="Latest Posts" />
-
-			<Gallery
-				items={firstTwoBlogPosts}
-				mdGridCols="md:grid-cols-2"
-				lgGridCols="lg:grid-cols-2"
-				pageKey="posts"
-				slugKey="slug"
-				linkKey="url"
-			/>
-
-			<GroupTitle title="Reading List" />
-
-			<MiniCardView
-				items={firstThreeReadingListPosts}
-				pageKey="reading-list"
-				linkKey="url"
-				tagsKey="tags"
-			/>
+			<h1 className="text-2xl font-bold">Hello World</h1>
 		</div>
 	);
-}
+};
+
+export default Page;
