71f572ee45df77c3af1b5225930e3ff2297e3e23
Greg Robleto <greg@robleto.com>
2025-02-13 20:57:16 -0500

    Refactor PostsPage component to use ParentComponent for improved structure; simplify PostsContainer integration

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -2,44 +2,32 @@ import React from "react";
 import { fetchNotionData } from "@/lib/notionContentFetcher";
 import PageHeader from "@/app/_components/layout/page/PageHeader";
 import { sortByPinnedAndDate } from "@/utils/sortItems";
-import PostsContainer from "@/app/_components/views/common/Container"; // Import the updated PostsContainer
 import Lists from "../_components/views/list/List"; // Import Lists component
 import Gallery from "../_components/views/gallery/Gallery"; // Import Gallery component
 
-export default async function PostsPage() {
-	const { pageContent, listItems } = await fetchNotionData({
-		databaseId: process.env.NOTION_POSTS_DB_ID!,
-		pageId: process.env.NOTION_POSTS_PAGE_ID!,
-		entryType: "posts", // Specify the entry type for mapping
-	});
-
-	const sortedItems = sortByPinnedAndDate(listItems, "pubdate");
-
-	return (
-		<div>
-			<PageHeader title="Posts" icon="posts" pageContent={pageContent} />
-				
-			{/* Pass sortedItems and components to the client-side PostsContainer */}
-			<PostsContainer
-				sortedItems={sortedItems}
-				ListComponent={Lists}
-				GalleryComponent={Gallery}
-				listProps={{
-					linkKey: "url",
-					pubDateKey: "pubdate",
-					pageKey: "posts",
-					tagsKey: "tags",
-					slugKey: "slug",
-				}}
-				galleryProps={{
-					lgGridCols: "lg:grid-cols-2",
-					linkKey: "url",
-					pubDateKey: "pubdate",
-					pageKey: "posts",
-					tagsKey: "tags",
-					slugKey: "slug",
-				}}
-			/>
-		</div>
-	);
+interface ParentComponentProps {
+  ListComponent: React.ComponentType;
+  GalleryComponent: React.ComponentType;
 }
+
+const ParentComponent: React.FC<ParentComponentProps> = ({ ListComponent, GalleryComponent }) => (
+  <div>
+    <ListComponent />
+    <GalleryComponent />
+  </div>
+);
+
+const PostsContainer = ({ sortedItems, ListComponent, GalleryComponent, listProps, galleryProps }) => {
+    return (
+        <div>
+            <ListComponent items={sortedItems} {...listProps} />
+            <GalleryComponent items={sortedItems} {...galleryProps} />
+        </div>
+    );
+};
+
+const Page: React.FC = () => (
+  <ParentComponent ListComponent={Lists} GalleryComponent={Gallery} />
+);
+
+export default Page;
